3dda164efb2ccb1e0e9a9cb5c5c0f9f7eff87d5b
Gerard Montane <gmontanee@gmail.com>
2019-07-01 14:45:45 +0200

    Connected documents

--- main.js
+++ main.js
@@ -1,2 +1,23 @@
 'use strict';
 
+function main () {
+  var main = document.querySelector('site-main');
+
+  function buildDom() {
+
+  };
+
+  function createSplashScreen() {
+    
+  };
+
+  function createGameScreem() {
+
+  };
+
+  function createGameOverScreen() {
+
+  };
+};
+
+window.addEventListener('load',main);
